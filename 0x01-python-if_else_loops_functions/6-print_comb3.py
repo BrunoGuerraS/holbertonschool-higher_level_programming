e4f58292a81cdd3545cf35685d91f8a1eb807d24
mr.b <bruno932151@gmail.com>
2021-12-21 17:34:32 -0500

    task6 was created

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+dig1 = 0
+dig2 = 1
+while dig1 <= 8:
+    while dig2 <= 9:
+        if dig1 != dig2:
+            print("{:d}".format(dig1), end='')
+            if dig1 != 8:
+                print("{:d}, ".format(dig2), end='')
+            else:
+                print("{:d}".format(dig2)
+        dig2 += 1
+    dig2 = dig1 + 1
+    dig1 += 1
