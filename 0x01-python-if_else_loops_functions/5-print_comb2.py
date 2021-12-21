a22df08010a53f5a7f521af167524675bd7cabac
mr.b <bruno932151@gmail.com>
2021-12-21 16:43:52 -0500

    task5 was chmod

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 i = 0
 while i < 100:
     if i != 99:
-        print("{:02d}, ".format(i), end="")
+        print("{:02d}, ".format(i), end='')
     else:
         print("{:02d}".format(i))
     i += 1
