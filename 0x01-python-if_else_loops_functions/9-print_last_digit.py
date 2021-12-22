28ed0bf266200b744aa7caa1ac4fb00167e3f49e
mr.b <bruno932151@gmail.com>
2021-12-21 19:28:57 -0500

    task9 was created

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        posnomb = number * -1
+        last = posnomb % 10
+    else:
+        last = number % 10
+    print("{:d}".format(last), end='')
