56764d43a245c28ef11ed9edc20c176abcd4ac3a
mr.b <bruno932151@gmail.com>
2021-12-21 20:23:39 -0500

    task12 was update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    i = 1
-    for i in range(101):
+    for i in range(1,101):
         if i == 100:
             print("{:s} ".format("Buzz"), end='')
         elif i % 3 == 0 or i % 5 == 0 or i % 3 == 0 and i % 5 == 0:
