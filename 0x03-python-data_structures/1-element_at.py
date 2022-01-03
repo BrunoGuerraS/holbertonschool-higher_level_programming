71f8eb6d0aae112b3856072ed91ed761295e8a90
mr.b <bruno932151@gmail.com>
2022-01-03 12:07:24 -0500

    task 1 was change, conditionals if and elif was added

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    b = my_list[idx]
-    return(b)
+    if idx < 0:
+        return(None)
+    elif idx > (len(my_list) - 1):
+        return(None)
+    else:
+        b = my_list[idx]
+        return(b)
