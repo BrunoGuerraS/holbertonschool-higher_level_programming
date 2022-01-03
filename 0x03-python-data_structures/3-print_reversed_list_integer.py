c1e90496d9aa1edb034f1cc3355fb1a486219686
mr.b <bruno932151@gmail.com>
2022-01-03 13:26:40 -0500

    task3 was update in line 3, 4

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
+    if my_list == None:
+        return(None)
+    elif len(my_list) == 0:
         return(None)
     else:
         a = len(my_list) - 1
