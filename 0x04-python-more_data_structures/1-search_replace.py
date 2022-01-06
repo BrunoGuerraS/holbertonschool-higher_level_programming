8eb089c717aaf1537d04578a882750dda211ba67
mr.b <bruno932151@gmail.com>
2022-01-05 22:27:42 -0500

    task1 was created

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    if my_list == 0 or my_list == None:
+        return None
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
