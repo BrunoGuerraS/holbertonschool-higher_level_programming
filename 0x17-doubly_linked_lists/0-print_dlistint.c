b661083af1507ada60c1817ddc7be7ab86f8e5b6
mr.b <bruno932151@gmail.com>
2022-01-06 23:47:34 -0500

    task0 was update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,5 @@
-#include "lists.h"
+#include <stdio.h>
+#include "list.h"
 /**
  * print_dlistint - print list
  * @dlistint_t: pointer
@@ -8,6 +9,6 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	if (h == NULL)
 		return (0);
-	dprintf(1, "%d\n", h->n);
+	printf(1, "%d\n", h->n);
 	return (1 + print_dlistint(h->next));
 }
