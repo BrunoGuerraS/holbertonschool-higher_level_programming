0e46725eb09dece7014d8c744965dd03fdda462d
mr.b <bruno932151@gmail.com>
2022-01-06 23:26:46 -0500

    task12 was created

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    num = 0
+    pos = 0
+    curr = 0
+    after = 0
+    convert = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    if type(roman_string) != str or roman_string is None:
+        return 0
+    lg = len(roman_string)
+    for key in roman_string:
+        curr = convert.get(key)
+        after = convert.get(roman_string[pos + 1]) if pos + 1 < lg else 0
+        if after > curr:
+            num += after - 1
+            break
+        if key in convert.keys():
+            num += convert.get(key)
+        pos += 1
+    return num
