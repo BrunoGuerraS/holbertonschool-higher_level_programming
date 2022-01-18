60b958ed000a7f8fe4803939b104bb8cf7832de4
mr.b <bruno932151@gmail.com>
2022-01-18 18:55:22 -0500

    update task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,10 +5,9 @@
 
 
 class Square:
-    """Square is a class to handler a square 
+    """Square is a class to handler a square
+
     """
-
-
     def __init__(self, size=0):
         """size of the elements
         """
