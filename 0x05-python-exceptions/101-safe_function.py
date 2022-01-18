f658e56d7e2f43864843239b7bfed9d8bd3a8a00
mr.b <bruno932151@gmail.com>
2022-01-17 21:56:13 -0500

    task8 was created

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,7 @@
+def safe_function(fct, *args):
+    import sys
+    try:
+        return fct(*args)
+    except Exception as value:
+        print("Exception: {}".format(value), file=sys.stderr)
+        return None
