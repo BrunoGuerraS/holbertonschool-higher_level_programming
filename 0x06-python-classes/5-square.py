68fdae561d4356cfd29308758780e32e84f25883
mr.b <bruno932151@gmail.com>
2022-01-18 23:30:42 -0500

    task5 was update

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -14,6 +14,8 @@ class Square:
 
     def my_print(self):
         """prints in stdout the square with the character"""
+        if self.__size == 0:
+            print("")
         for i in range(self.__size):
             print("#" * self.__size)
 
