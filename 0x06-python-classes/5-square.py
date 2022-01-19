3c0fa936eda8adfb9fdf1c9c637675873ee910f7
mr.b <bruno932151@gmail.com>
2022-01-18 23:24:18 -0500

    task5 was created

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""this is task4"""
+
+
+class Square:
+    """Squere hangler square"""
+    def __init__(self, size=0):
+        """size is size of elements"""
+        self.__size = size
+
+    def area(self):
+        """genera area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints in stdout the square with the character"""
+        for i in range(self.__size):
+            print("#" * self.__size)
+
+    @property
+    def size(self):
+        """ size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, s):
+        """ size setter"""
+        if not isinstance(s, int):
+            raise TypeError("size must be an integer")
+        if s < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = s
