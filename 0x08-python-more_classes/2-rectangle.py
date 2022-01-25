62579388fcd1ebf99dfa3e793945c6b7c779973e
mr.b <bruno932151@gmail.com>
2022-01-24 19:12:24 -0500

    task2 was update

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -16,6 +16,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        '''setter width'''
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -29,6 +30,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        '''setter height'''
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
