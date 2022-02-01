94db2341fc196cbfe99db763133c81fc7b18e495
mr.b <bruno932151@gmail.com>
2022-02-01 18:51:36 -0500

    task1 update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''writes a string to a text file'''
+
+
 def write_file(filename="", text=""):
+    ''' function write_file'''
     with open(filename, 'w', encoding="UTF-8") as file:
         write_f = file.write(text)
         return write_f
