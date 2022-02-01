5f3daf941caa247fe3f493ed377ee6c33a0ea6ef
mr.b <bruno932151@gmail.com>
2022-02-01 18:56:31 -0500

    task0 update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+''' that reads a text file'''
+
+
 def read_file(filename=""):
-    with open(filename, encoding="UTF-8") as text:
+    ''' read_file function'''
+    with open(filename, 'r+', encoding="UTF-8") as text:
         read_data = text.read()
         print(read_data)
