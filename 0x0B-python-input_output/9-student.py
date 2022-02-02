0fb295e5c55c8cf90b80fb2006a5daa883ccabfb
mr.b <bruno932151@gmail.com>
2022-02-01 21:17:35 -0500

    task9 was created

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' defines a student '''
+
+
+class Student:
+    ''' class student '''
+    def __init__(self, first_name, last_name, age):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__age = age
+
+    def to_json(self):
+        return self.__dict
