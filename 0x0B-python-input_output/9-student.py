b44a2dc2f443a24dc5bb26773db0236f9c1c51d6
mr.b <bruno932151@gmail.com>
2022-02-01 21:18:56 -0500

    task9 update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,9 +5,9 @@
 class Student:
     ''' class student '''
     def __init__(self, first_name, last_name, age):
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-        return self.__dict
+        return self.__dict__
