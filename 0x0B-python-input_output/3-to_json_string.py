a5aa4dfdd716c17b043b1e685a9f13e73f37a61a
mr.b <bruno932151@gmail.com>
2022-02-01 19:23:56 -0500

    task3 was created

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' returns the JSON representation of an object '''
+
+
+import json
+""" Import module json for use method dumps 
+    for serialized
+"""
+
+
+def to_json_string(my_obj):
+    ''' function  to_json_string'''
+    return json.dumps(my_obj)
