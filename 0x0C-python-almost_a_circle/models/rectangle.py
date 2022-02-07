f85b9575fb9484af8be1ddad79bca76285e99a7d
mr.b <bruno932151@gmail.com>
2022-02-07 14:06:07 -0500

    task reactangle update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,16 +1,53 @@
-#!/usr/bin/python3
+# /usr/bin/python3
 from base import Base
 ''' we create a class reactangle'''
 
 
 class Rectangle(Base):
    ''' class rectangle'''
-   def __init__(self, width, height, x=0, y=0, id=None):
-       ''' constructor method'''
-       super().__init__(id)
-       self.width = width
-       self.height = height
-       self.x = x
-       self.y = y
+    def __init__(self, width, height, x=0, y=0, id=None):
+        ''' constructor method'''
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+    
+    @property
+    def  width(self):
+        ''' '''
+        return self.__width
+
+    @width.setter
+    def width(self. value):
+        ''' '''
+        self.__width = value
+
+    @propery
+    def height(self):
+        ''' ''' 
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        ''' '''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__x = value
+
 
 
