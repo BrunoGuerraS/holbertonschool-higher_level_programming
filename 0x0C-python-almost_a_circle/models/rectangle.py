71eb709bd1726f5db689c2171df83278facc86ae
mr.b <bruno932151@gmail.com>
2022-02-07 15:08:15 -0500

    task 2 was update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ from base import Base
 
 
 class Rectangle(Base):
-   ''' class rectangle'''
+    ''' class rectangle'''
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' constructor method'''
         super().__init__(id)
