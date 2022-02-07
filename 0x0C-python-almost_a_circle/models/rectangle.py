d5c5d9ca13b9aa55cd63b5543371eba9b0433e6b
mr.b <bruno932151@gmail.com>
2022-02-07 17:52:14 -0500

    task2 update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,36 +15,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        ''' '''
+        ''' getter attribute width'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        ''' '''
+        ''' setter attribute width'''
         self.__width = value
 
     @property
     def height(self):
-        ''' '''
+        ''' getter attribute height'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        ''' setter attribute height'''
         self.__height = value
 
     @property
     def x(self):
-        ''' '''
+        ''' getter attribute x'''
         return self.__x
 
     @x.setter
     def x(self, value):
+        ''' setter attribute x'''
         self.__x = value
 
     @property
     def y(self):
+        ''' getter attribute y'''
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.__x = value
+        ''' setter attribute y'''
+        self.__y = value
