d53a5d25bb0780fa71402502041f99e3be5346fb
mr.b <bruno932151@gmail.com>
2022-02-07 21:40:24 -0500

    task10 was created

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' class Square '''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''this class give us a square '''
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        ''' the size '''
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        ''' getter size'''
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return a string with rectangle to stdout"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
