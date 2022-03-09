19f37fb6eda01043966424f77df2d022973757a1
b <bruno932151@gmail.com>
2022-03-09 12:47:12 -0800

    update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,3 @@
 -- a script that creates the MySQL server user  --
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
-ALTER USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on hbtn_0d_2.* TO user_0d_2@localhost;
