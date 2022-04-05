b497cc3dae2033816920d010e091f1121053ef60
mr.b <bruno932151@gmail.com>
2022-04-05 16:05:47 -0500

    task6 fixed

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      this.print();
+    } else {
+      const area = [];
+      for (let i = 0; i < this.width; i++) {
+        area.push('C');
+      }
+      for (let j = 0; j < this.height; j++) {
+        console.log(area.join(''));
+      }
+    }
+  }
+}
+module.exports = Square;
