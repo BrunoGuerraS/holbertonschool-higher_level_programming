4b08c3961b80492366695d5baad38823e571bbcc
mr.b <bruno932151@gmail.com>
2022-04-04 22:57:33 -0500

    task11 was created

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (!process.argv[2] || parseInt(process.argv[2]) === 1) {
+  console.log(0);
+} else {
+  const list = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    list.push(process.argv[i]);
+  }
+  list.sort(function (a, b) {
+    return (b - a);
+  });
+  console.log(list[0]);
+}
