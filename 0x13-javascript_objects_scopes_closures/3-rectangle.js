bbd471ea90bd74c98ac4af41ee8f8dc6c5d0d388
mr.b <bruno932151@gmail.com>
2022-04-05 13:00:32 -0500

    task3 was created

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const area = [];
+    for (let i = 0; i < this.width; i++) {
+      area.push('X');
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(area.join(''));
+    }
+  }
+}
+module.exports = Rectangle;
