a0ff4e5b5fae2ef074f810892c472289516638bb
mr.b <bruno932151@gmail.com>
2022-04-04 21:46:37 -0500

    task8 was created

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
+const size = parseInt(process.argv[2]);
 const square = [];
-if (isNaN(x)) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < x; i++) {
-    square.push('x');
-  }
+
+if (isNaN(size)) console.log('Missing size');
+else {
+  for (let i = 0; i < size; i++) square.push('X');
   square.forEach(() => console.log(square.join('')));
 }
