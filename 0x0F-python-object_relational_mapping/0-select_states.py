00a0bcbd2b6b0444f3f3f8be0546833c52d9d029
b <bruno932151@gmail.com>
2022-04-07 14:42:50 -0700

    we created script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+def conection():
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], 
+                             passwd=argv[2], db=argv[3], charset="utf8")
+    except Exception:
+        print("can't connect")
+        return 0
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+conection()
