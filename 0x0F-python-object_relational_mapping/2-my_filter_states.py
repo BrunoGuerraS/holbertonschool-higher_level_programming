f16dbade356e17a3f1dbfffd0033ded9a1d23c57
mr.b <bruno932151@gmail.com>
2022-04-10 13:46:10 -0500

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,7 @@ from sys import argv
 def conection():
 
     try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], 
                              passwd=argv[2], db=argv[3], charset="utf8")
     except Exception:
         print("can't connect")
