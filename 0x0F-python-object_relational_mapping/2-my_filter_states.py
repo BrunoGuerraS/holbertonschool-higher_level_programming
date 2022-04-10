61d8a707e4138f27f750a8c45961b4029fda00e5
mr.b <bruno932151@gmail.com>
2022-04-10 14:07:05 -0500

    fixing task2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 ''' we start to work with MySQLdb to handle data bases'''
-import MySQLdb
-from sys import argv
 
 
 def conection():
     ''' this function connect to databae '''
+
+    import MySQLdb
+    from sys import argv
+
     try:
         db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                              passwd=argv[2], db=argv[3], charset="utf8")
@@ -14,15 +16,14 @@ def conection():
         return 0
 
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    instruc = "SELECT * FROM states WHERE BINARY name=%s ORDER BY id"
+    cur.execute(instruc, (argv[4],))
     rows = cur.fetchall()
     for row in rows:
-        if row[1] == argv[4] and row[1][0] == 'A':
-            print(row)
+        print(row)
     cur.close()
     db.close()
 
 
 if __name__ == '__main__':
     conection()
-
