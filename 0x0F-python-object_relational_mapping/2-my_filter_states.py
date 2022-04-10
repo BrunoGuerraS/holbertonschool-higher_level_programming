6a5f6bd2c332164d27d270e58d1e4cbafeff0293
mr.b <bruno932151@gmail.com>
2022-04-10 13:45:16 -0500

    pycodestyle in task2 was passe

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,10 +2,11 @@
 import MySQLdb
 from sys import argv
 
+
 def conection():
 
     try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], 
+        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                              passwd=argv[2], db=argv[3], charset="utf8")
     except Exception:
         print("can't connect")
