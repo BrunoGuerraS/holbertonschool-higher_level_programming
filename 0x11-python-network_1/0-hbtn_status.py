6a54b74e019fd9aeb7d6e141adc075ea4de45d02
mr.b <bruno932151@gmail.com>
2022-04-22 22:48:36 -0500

    this task is about we mush fetching url page

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" fetching https://intranet.hbtn.io/status """
+import urllib.request
+
+
+def fetching(page):
+    """ we're fetching url page"""
+    with urllib.request.urlopen(page) as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+
+
+if __name__ == '__main__':
+    fetching('https://intranet.hbtn.io/status')
