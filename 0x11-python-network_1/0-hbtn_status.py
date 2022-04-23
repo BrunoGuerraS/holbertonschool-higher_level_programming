6318ed6832efb5216ca5a497368feeb6511454db
mr.b <bruno932151@gmail.com>
2022-04-22 22:54:04 -0500

    task0 update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,11 +7,12 @@ def fetching(page):
     """ we're fetching url page"""
     with urllib.request.urlopen(page) as response:
         html = response.read()
-        print('Body response:')
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode("utf-8"))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
 
 
-if __name__ == '__main__':
-    fetching('https://intranet.hbtn.io/status')
+if __name__ == "__main__":
+    fetching("https://intranet.hbtn.io/status")
+    
