d1ca3b18e510629e05f0bac5c221d900fb6e24ac
mr.b <bruno932151@gmail.com>
2022-04-25 16:36:50 -0500

    task3 debugging

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,23 +3,21 @@
 taking in a URL, sends a request to the URL
 and displays the body of the response utf-u
 """
-from encodings import utf_8
-from sys import argv
 
 
 def triggerFunc():
     """func generate takes information in URL"""
     import urllib.request
     import urllib.error
-
+    from sys import argv
     urlpath = argv[1]
 
     try:
         with urllib.request.urlopen(urlpath) as response:
-            getdata = response.read().decode("utf_8")
-            print(getdata)
+            body = response.read()
+            print(body.decode('utf-8'))
     except urllib.error.HTTPError as thiserror:
-        print("Error code: {}".format(thiserror))
+        print('Error code: {}'.format(thiserror.code))
 
 
 if __name__ == "__main__":
