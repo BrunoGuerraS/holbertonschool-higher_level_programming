97e249b6b7b2cc1fb0d41bdf16c6e3011a6ee99c
mr.b <bruno932151@gmail.com>
2022-04-28 23:19:24 -0500

    adding a func

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+display values
+"""
 import requests
 from sys import argv
-url = argv[1]
-value = 'X-Request-Id'
-html = requests.get(url)
-print(html.headers.get(value))
+
+
+def foo():
+    """displays the value of the variable"""
+    url = argv[1]
+    value = 'X-Request-Id'
+    html = requests.get(url)
+    print(html.headers.get(value))
+
+
+foo()
