bbab10c22be1b2bb054268dfc1f047b9b2c79d0e
mr.b <bruno932151@gmail.com>
2022-05-18 19:22:54 -0500

    fixing task2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
+const axios = require('axios');
+const url = process.argv[2];
 
+console.log(url);
+axios.get(url)
+  .then(res => {
+    console.log('code: ', res.status);
+  })
+  .catch(err => {
+    console.log('code :', err.response.status);
+  });
