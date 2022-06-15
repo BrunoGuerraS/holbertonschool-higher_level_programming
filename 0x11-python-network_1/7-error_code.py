f309f40db40fb3e13acab29c48ca08fadda84671
BrunoGS <bruno932151@gmail.com>
2022-06-15 12:30:57 -0500

    fixin7task

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,19 +5,14 @@ to the URL and displays the body of the response
 """
 
 
-import requests
-from sys import argv
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-
-def showbody():
     url = argv[1]
 
     response = requests.get(url)
     if response.status_code >= 400:
-        print("Error code:", res.status_code)
+        print("Error code:", response.status_code)
     else:
         print(response.text)
-
-
-if __name__ == "__main__":
-    showbody()
