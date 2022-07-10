0be7deb6a410e12fe8af50e7bed47568ae36c3d6
BrunoGS <bruno932151@gmail.com>
2022-07-10 17:26:27 -0500

    task advance update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-//script that imports a dictionary of occurrences
-//by user id and computes a dictionary 
-//of user ids by occurrence.
 const { dict } = require('./101-data');
 
 const currentValue = Object.entries(dict).reduce((acc, [key, value]) => {
